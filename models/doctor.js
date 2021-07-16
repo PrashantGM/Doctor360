b51a4db8e141039569d2259d5b2016e79a5cc7e9
PrashantGM <gmprem14@gmail.com>
2021-07-17 01:38:39 +0545

    validation for doctor registration done

--- models/doctor.js
+++ models/doctor.js
@@ -1,38 +1,58 @@
-const mongoose=require('mongoose');
+const mongoose = require('mongoose');
+const yup = require('yup');
+const DoctorSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        autoindex: true,
+        required: true
 
-const DoctorSchema=new mongoose.Schema({
-    name:{
-        type:String,
-        required:true
     },
-    email:{
-        type:String,
-        unique:true,
-        autoindex:true,
-        required:true
-       
-    },
-    mobile:{
-        type:String,
-        unique:true,
-        required:true
+    mobile: {
+        type: String,
+        unique: true,
+        required: true
     },
 
-    gender:{
-        type:String,
-        required:true
+    gender: {
+        type: String,
+        required: true
     },
-    specialization:{
-        type:String,
-        required:true
+    specialization: {
+        type: String,
+        required: true
     },
-    documentImage:{
-        type:String,
+    documentImage: {
+        type: String,
         default: "no-photo.jpg",
-    },  
-    password:{
-        type:String,
-        required:true
+    },
+    password: {
+        type: String,
+        required: true
     }
 })
-module.exports=new mongoose.model('Doctor',DoctorSchema);
+const validateDoctor = (doctor) => {
+    const schema = yup.object().shape({
+        name: yup.string().required('Please input your name'),
+        email: yup.string().required(),
+        mobile: yup.string().required(),
+        gender: yup.string().required(),
+        specialization: yup.string().required(),
+        documentImage: yup.string(),
+        password: yup.string().required(),
+    })
+    return schema
+        .validate(doctor)
+        .then(doctor=>doctor)
+        .catch((error) =>{ 
+         return{
+            message:error.message 
+                }   
+        });
+}
+exports.Doctor = new mongoose.model('Doctor', DoctorSchema);
+exports.validateDoctor=validateDoctor;
--- routes/doctor.js
+++ routes/doctor.js
@@ -1,18 +1,18 @@
 const express=require('express');
 const router=express.Router();
-const Doctor=require('../models/doctor');
+const {Doctor,validateDoctor}=require('../models/doctor');
 
-router.post("/register",(req,res)=>{
-    
+router.post("/register",async (req,res)=>{
+  const error= await validateDoctor(req.body);
+  if(error.message) res.status(400).send(error.message);
   var dData=new Doctor(req.body);
   dData.save()
-  .then(()=>{
-    res.status(201).json({success:true});
-  })
-  .catch((e)=>{
-    res.status(500).json({message:e});
-    
-  })
+  // .then(()=>{
+  //   res.status(201).json({success:true});
+  // })
+  // .catch((e)=>{ 
+  //   res.status(500).json({message:e});
+  // })
 })
 router.get("/login",function(req,res){
   Doctor.find()
