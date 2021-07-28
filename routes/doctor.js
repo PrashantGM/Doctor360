897393fea93eca09f562ea43e413ad5f7f5f7731
PrashantGM <gmprem14@gmail.com>
2021-07-28 21:02:35 +0545

    added update method for doctor

--- routes/doctor.js
+++ routes/doctor.js
@@ -57,4 +57,61 @@ router.get("/view",async function(req,res){
   })
 })
 
+//display doctor details by id
+router.get("/view/:id",async function(req,res){
+  const id=req.params.id;
+  const logDoctor= await Doctor.findOne({_id:id})
+    .then((result)=>{
+      res.status(201).json({success:"true",data:result});
+    })
+    .catch((e)=>{
+      res.status(500).json({success:"false",message:"Error loading results"});
+  })
+})
+
+
+router.put('/updateprofile/:id',function(req,res){
+  const name=req.body.name;
+  const address=req.body.address;
+  const email=req.body.email;
+  const mobile=req.body.mobile;
+  const gender=req.body.gender;
+  const specialization=req.body.specialization;
+  const qualification=req.body.qualification;
+  const hid=req.body.id;
+  Hospital.updateOne({_id:hid},{name:name,address:address,email:email,mobile:mobile,gender:gender,specialization:specialization,qualification:qualification})
+  .then(function(result){
+      res.status(200).json({success:"true",message:`Profile of Dr. ${name}  Updated`})
+  })
+  .catch(function(e){
+      res.status(500).json({success:"false",message:e});
+  })
+});
+
+
+//for password update
+router.get("/viewpassword/:id",async function(req,res){
+  const id=req.params.id;
+  const logDoctor= await Doctor.findOne({_id:id})
+    .then((result)=>{
+      res.status(201).json({success:"true",data:result.password});
+    })
+    .catch((e)=>{
+      res.status(500).json({success:"false",message:"Error loading results"});
+  })
+})
+router.put('/updatepassword/:id',function(req,res){
+ 
+  const password=req.body.password;
+  const hid=req.body.id;
+  Hospital.updateOne({_id:hid},{password:password})
+  .then(function(result){
+      res.status(200).json({success:"true",message:"Password Changed Successfully"})
+  })
+  .catch(function(e){
+      res.status(500).json({success:"false",message:e});
+  })
+});
+
+
 module.exports=router;
